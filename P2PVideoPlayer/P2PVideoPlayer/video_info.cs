b692bf65800e5d7c8622a4336b595dd77a3de217
YE ZHIZHEN <yezhizhenjiakang@gmail.com>
2016-02-23 14:33:06 +0800

    Changes to database
    Now we can freely use xml as database media.

--- P2PVideoPlayer/P2PVideoPlayer/video_info.cs
+++ P2PVideoPlayer/P2PVideoPlayer/video_info.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace WpfApplication1
 {
@@ -14,31 +15,54 @@ namespace WpfApplication1
         public String path { get; set; }
 
         //initialize a video object
-        public video_info() {
-            fileName = "";
-            title = "";
-            author = "";
-            album = "";
+        public video_info()
+        {
+            fileName = "N/A";
+            title = "N/A";
+            author = "N/A";
+            album = "N/A";
+            path = "N/A";
         }
-
+                    
+        
         //load a line of input
-        //'1.wav' 'title' 'author' 'album'
-        public void load(String text) {
-            char[] separatingChars = {'\''};
-            char[] separatingChars1 = { '\\','/' };
-            string[] words = text.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
-            if (words.Length == 7)
+        //Filename:'1.wav' 'title' 'author' 'album'
+        public void load(XmlNode video) {
+            if (video["path"] != null)
             {
-                path = words[0];
-                string[] name = words[0].Split(separatingChars1, System.StringSplitOptions.RemoveEmptyEntries);
-                fileName = name[name.Length-1];
-                title = words[2];
-                author = words[4];
-                album = words[6];
+                path = video["path"].InnerText;
+                char[] separatingChars1 = { '\\', '/' };
+                string[] Names = path.Split(separatingChars1, System.StringSplitOptions.RemoveEmptyEntries);
+                fileName = Names[Names.Length - 1];
             }
-            else {
-                return;
+            if (video["title"] != null)
+            {
+                path = video["title"].InnerText;
             }
+            if (video["author"] != null)
+            {
+                path = video["author"].InnerText;
+            }
+            if (video["album"] != null)
+            {
+                path = video["album"].InnerText;
+            }
+            //char[] separatingChars = {'\''};
+            //char[] separatingChars1 = { '\\','/' };
+            //string[] words = text.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
+            //if (words.Length == 7)
+            //{
+            //    path = words[0];
+            //    string[] name = words[0].Split(separatingChars1, System.StringSplitOptions.RemoveEmptyEntries);
+            //    fileName = name[name.Length-1];
+            //    title = words[2];
+            //    author = words[4];
+            //    album = words[6];
+            //}
+            //else {
+            //    return;
+            //}
+
         }
 
         public void readFromAddr(String text)
