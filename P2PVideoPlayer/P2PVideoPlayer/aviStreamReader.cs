76748225d1952a558dfbdf48513f7b7ebb283e29
Kelly <xingyue.kelly@gmail.com>
2016-02-29 21:14:01 +0800

    have no idea what I have uploaded...

---
+++ P2PVideoPlayer/P2PVideoPlayer/aviStreamReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Drawing;
+using AForge.Video.FFMPEG;
+
+
+namespace WpfApplication1
+{
+    public class AVIStreamReader
+    {
+        private String path;
+        private VideoFileReader reader;
+        private VideoFileWriter writer;
+        private int height;
+        private int width;
+
+        public AVIStreamReader(String path)
+        {
+            this.path = path;
+            reader = new VideoFileReader();
+            reader.Open(path);
+            writer = new VideoFileWriter();
+            height = reader.Height;
+            width = reader.Width;
+        }
+
+        ~AVIStreamReader()
+        {
+            reader.Close();
+        }
+
+        public void readStream(String name,int num)
+        {
+            writer.Open(name, width, height);
+            for (int i = 0; i < num; i++)
+            {
+                writer.WriteVideoFrame(reader.ReadVideoFrame());
+            }
+            writer.Close();
+        }
+
+    }
+}
--- P2PVideoPlayer/P2PVideoPlayer/video_info.cs
+++ P2PVideoPlayer/P2PVideoPlayer/video_info.cs
@@ -4,7 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
-using static Microsoft.VisualBasic.Interaction;
+//using static Microsoft.VisualBasic.Interaction;
+
 
 namespace WpfApplication1
 {
@@ -65,9 +66,9 @@ namespace WpfApplication1
             string[] name = text.Split(separatingChars1, System.StringSplitOptions.RemoveEmptyEntries);
             fileName = name[name.Length - 1];
             //ask for title, author, album
-            title = InputBox("What is the title of the music?",fileName+": Title","N/A");
-            author = InputBox("Who is the author of the music?", fileName + ": Author", "N/A");
-            album = InputBox("What is the album of the music?", fileName + ": Album", "N/A");
+            //title = InputBox("What is the title of the music?",fileName+": Title","N/A");
+            //author = InputBox("Who is the author of the music?", fileName + ": Author", "N/A");
+            //album = InputBox("What is the album of the music?", fileName + ": Album", "N/A");
             path = text;
             XmlNodeList l = doc.SelectNodes("/Karaoke/video", media_info.man);
             XmlNode last = l[l.Count - 1];
