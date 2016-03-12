cfb65d6fb95d7bd673fcc819d047e94b6f1352b9
Kelly <xingyue.kelly@gmail.com>
2016-03-12 19:04:10 +0800

    add Pacakge and Connector class

---
+++ P2PVideoPlayer/P2PVideoPlayer/Connector.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WpfApplication1
+{
+    class Connector
+    {
+        public UdpClient conn;
+        public String ip;
+        public int port;
+        public byte[] buffer;
+        public Connector() {
+            conn = new UdpClient();
+            buffer = new byte[0];
+        }
+        public void setDeset(String addr,int portnum){
+            ip=addr;
+            port=portnum;
+        }
+        public void connect() {
+            conn.Connect(ip, port);
+        }
+        public void send(Package pack) {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter bformatter = new BinaryFormatter();
+            bformatter.Serialize(stream, pack);
+            byte[] b;
+            using (BinaryReader br = new BinaryReader(stream))
+            {
+                b = br.ReadBytes(stream.Length);
+            }
+            byte[] a = BitConverter.GetBytes(b.Length);
+            byte[] c = new byte[a.Length + b.Length];
+            System.Buffer.BlockCopy(a, 0, c, 0, a.Length);
+            System.Buffer.BlockCopy(b, 0, c, a.Length, b.Length);
+            conn.Send(c, c.Length);
+        }
+        public Package recv() {
+            byte[] len = new byte[4];
+            int length = 0,sum = 0;
+            MemoryStream stream = new MemoryStream();
+            while (buffer.Length > 0)
+            {
+                if(length == 0 && buffer.Length == 0){buffer = conn.Receive();}
+                if (length == 0)
+                {
+                    System.Buffer.BlockCopy(buffer, 0, len, 0, 4);
+                    length = BitConverter.ToInt32(len, 0);
+                    byte[] buffer1 = new byte[buffer.Length - 4];
+                    System.Buffer.BlockCopy(buffer, 4, buffer1, 0, buffer.Length - 4);
+                    buffer = new byte[buffer1.Length];
+                    System.Buffer.BlockCopy(buffer1, 0, buffer, 0, buffer.Length);
+                }
+                if (buffer.Length + sum < length)
+                {
+                    stream.Write(buffer,0,buffer.Length);
+                    buffer = new byte[0];
+                }
+                else
+                {
+                    stream.Write(buffer,0,length - sum);
+                    byte[] buffer1 = new byte[buffer.Length - (length - sum)];
+                    System.Buffer.BlockCopy(buffer, length - sum, buffer1, 0, buffer1.Length);
+                    buffer = new byte[buffer1.Length];
+                    System.Buffer.BlockCopy(buffer1, 0, buffer, 0, buffer.Length);
+                    bformatter = new BinaryFormatter();
+                    return (Package)bformatter.Deserialize(stream);
+                }
+            }
+        }
+    }
+}
---
+++ P2PVideoPlayer/P2PVideoPlayer/Package.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace WpfApplication1
+{
+    [Serializable()]
+    class Package: ISerializable
+    {
+        public String from;
+        public String to;
+        public String type;
+        public List<Tuple<String,int>> connectList;
+        public String video;
+        public String audio;
+        public String danmu;
+        public List<Tuple<String, int>> header;
+
+        public Package(String type_){
+            from = "";
+            to = "";
+            type = type_;
+            connectList = new List<Tuple<String, int>>();
+            header = new List<Tuple<String, int>>();
+            video = "";
+            audio = "";
+            danmu = "";
+        }
+
+        public Package(SerializationInfo info, StreamingContext ctxt)
+        {
+          from = (String)info.GetValue("from", typeof(String));
+          to = (String)info.GetValue("to", typeof(String));
+          type = (String)info.GetValue("type", typeof(String));
+          video = (String)info.GetValue("video", typeof(String));
+          audio = (String)info.GetValue("audio", typeof(String));
+          danmu = (String)info.GetValue("danmu", typeof(String));
+          connectList = (List<Tuple<String, int>>)info.GetValue("connectList", typeof(List<Tuple<String, int>>));
+          header = (List<Tuple<String, int>>)info.GetValue("header", typeof(List<Tuple<String, int>>));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("from", from);
+            info.AddValue("to", to);
+            info.AddValue("type", type);
+            info.AddValue("video", video);
+            info.AddValue("audio", audio);
+            info.AddValue("danmu", danmu);
+            info.AddValue("header", header);
+            info.AddValue("connectList", connectList);
+        }
+
+
+    }
+}
